cc79caaf8299a2cb7fae4b96841ba3e4018c4010
Shikha Gupta <Shikha@apples-MacBook-Pro-4.local>
2017-05-27 11:18:19 +0530

    Added Json response for get product

--- routes/product.js
+++ routes/product.js
@@ -4,16 +4,13 @@ var mongoose = require('mongoose')
 var Product = require('../models/product');
 
 router.get('/', function (req, res, next) {
-    res.render('product/add');
-    // var successMsg= req.flash('success')[0];
-    // Product.find(function (err, docs) {
-    //     var productChunks = [];
-    //     var chunkSize = 3;
-    //     for (var i = 0; i < docs.length; i += chunkSize) {
-    //         productChunks.push(docs.slice(i, i + chunkSize));
-    //     }
-    //     res.render('product/add', {title: 'Food Ordering App', products: productChunks});
-    // });
+
+    Product.find(function (err, products) {
+        if (err)
+            res.send(err);
+
+        res.json(products);
+    });
 });
 
 router.get('/addProduct', function (req, res, next) {
